f5115c6a6bc837752798adf749cea1d4140c6578
KanekiiCat <alex123viri@gmail.com>
2025-04-11 11:22:27 +0200

    feat(ComputerCard): Actualizar ComputerCard

--- src/main/java/edu/estatuas/examendam/ComputeCard.java
+++ src/main/java/edu/estatuas/examendam/ComputeCard.java
@@ -1,12 +1,19 @@
 package edu.estatuas.examendam;
 
+
 public class ComputeCard {
     
     ScoreCard scoreCard;
     GolfPlay golfPlay;
 
+    public ComputeCard(ScoreCard scoreCard, GolfPlay golfPlay){
+        this.scoreCard = scoreCard;
+        this.golfPlay = golfPlay;
+    }
+
+
     public void compute(){
-        
+        this.golfPlay.scoring(this.scoreCard);
     }
 
 }
